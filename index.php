20ca907414cbd9ad9c94754c7f95b598008ff3a6
Amanskywalker <ak47su30ac@gmail.com>
2016-01-09 17:52:45 +0530

    modified:   index.php
    	index is modulated into many php functions so it can be reused increasing the lines of codes
    
    	deleted:    styles.css
    
    	removed and modulated into two files
    	styles-text.css
    	styles-blog.css

--- index.php
+++ index.php
@@ -22,36 +22,20 @@
       <main class="mdl-layout__content">
         <div class="demo-blog__posts mdl-grid">
          <?php
-              include 'display/functions/otr.func.php';
+
+              include_once 'display/functions/otr.func.php';
               otr('hello','hello world','index.php');
 
-              include 'display/functions/amazing.func.php';
+              include_once 'display/functions/amazing.func.php';
               amazing('I couldn’t take any pictures but this was an amazing thing…','hello world','index.php');
-         ?>
-          <div class="mdl-card shopping mdl-cell mdl-cell--12-col">
-            <div class="mdl-card__media mdl-color-text--grey-50">
-              <h3><a href="entry.html">Shopping</a></h3>
-            </div>
-            <div class="mdl-card__supporting-text mdl-color-text--grey-600">
-              Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
-            </div>
-            <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-              <div class="minilogo"></div>
-              <div>
-                <strong>The Newist</strong>
-                <span>2 days ago</span>
-              </div>
-            </div>
-          </div>
-          <nav class="demo-nav mdl-cell mdl-cell--12-col">
-            <div class="section-spacer"></div>
-            <a href="entry.html" class="demo-nav__button" title="show more">
-              More
-              <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                <i class="material-icons" role="presentation">arrow_forward</i>
-              </button>
-            </a>
-          </nav>
+              
+              include_once 'display/functions/shopping.func.php';
+              shopping('hello','hello world','index.php');
+
+              include_once 'display/next.nav.func.php';
+              next_nav('index.php');
+              
+         ?> 
         </div>
     <?php
     	require 'footer.php';
